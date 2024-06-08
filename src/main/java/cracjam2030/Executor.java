ccf0bb0aaa772a72a6327b5194dbb501b6edb676
Amaz <sonicmephit@gmail.com>
2024-06-08 11:42:27 +0200

    Obsluga wybranych parametrow

--- src/main/java/cracjam2030/Executor.java
+++ src/main/java/cracjam2030/Executor.java
@@ -1,9 +1,11 @@
 package cracjam2030;
 
+import java.util.Arrays;
+
 //        -h -- help Wyświetla wszystkie opcje programu.
-//        -R1 [start date][end date] [path] Generuje Raport 1. Czas wykorzystany na projekt. Możliwość filtrowanie po start i end date. Należy podać ścieżkę całkowitą do pliku xls
-//        -R2 [start date][end date] [path] Generuje Raport 2. Czas wykorzystany przez dev na projekt. Możliwość filtrowanie po start i end date. Należy podać ścieżkę całkowitą do pliku xls
-//        -R3 [start date][end date] [path] Generuje Raport 1. Czas wykorzystany na zadania na projekt. Możliwość filtrowanie po start i end date. Należy podać ścieżkę całkowitą do pliku xls
+//        -R1 [path] [start date][end date] Generuje Raport 1. Czas wykorzystany na projekt. Możliwość filtrowanie po start i end date. Należy podać ścieżkę całkowitą do pliku xls
+//        -R2 [path] [start date][end date] Generuje Raport 2. Czas wykorzystany przez dev na projekt. Możliwość filtrowanie po start i end date. Należy podać ścieżkę całkowitą do pliku xls
+//        -R3 [path] [start date][end date] Generuje Raport 1. Czas wykorzystany na zadania na projekt. Możliwość filtrowanie po start i end date. Należy podać ścieżkę całkowitą do pliku xls
 //        -d Generowanie diagramu
 public class Executor {
 
@@ -11,8 +13,23 @@ public class Executor {
 
         if (args.length > 0) {
             System.out.printf("Function called with params");
+            if (Arrays.stream(args).anyMatch(s -> s.contains("-h"))){
+                System.out.printf("\n -h -help \n -R1 - Report for Report 1 \n -R2 - Report for Report 2 \n -R3 - Report for Report 3");
+            }
+            if (Arrays.stream(args).anyMatch(s -> s.contains("-R1"))){
+                String path = args[1];
+                System.out.printf("Running Report1 with path: " + path);
+            }
+            if (Arrays.stream(args).anyMatch(s -> s.contains("-R2"))){
+                String path = args[1];
+                System.out.printf("Running Report2 with path: " + path);
+            }
+            if (Arrays.stream(args).anyMatch(s -> s.contains("-R3"))){
+                String path = args[1];
+                System.out.printf("Running Report2 with path: " + path);
+            }
         }
-        System.out.printf("Function called without params");
+        else System.out.printf("Program called without params. Add some parameters, for help use parameter -h");
 
         }
     }
