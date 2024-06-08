addf7916a6a1331a2399f20ae2df5b058528c507
Amaz <sonicmephit@gmail.com>
2024-06-08 13:31:23 +0200

    Merge xlsloader i main

--- src/main/java/cracjam2030/Main.java
+++ src/main/java/cracjam2030/Main.java
@@ -1,59 +1,42 @@
 package cracjam2030;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static MessageLogger logger = new MessageLogger();
 
     public static void main(String[] args) throws IOException {
 
-
-//        Main.logger.addError("Pzykład dodania błędu");
-//        Main.logger.addLine("Przykład dodania linii raportu");
-//
-//
-//        // Press Alt+Enter with your caret at the highlighted text to see how
-//        // IntelliJ IDEA suggests fixing it.
-//        System.out.printf("Hello and welcome!");
-//
-//        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-//        for (int i = 1; i <= 5; i++) {
-//
-//            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-//            // for you, but you can always add more by pressing Ctrl+F8.
-//            System.out.println("i = " + i);
-//        }
-
         XmlLoader loader = new XmlLoader();
 
+        ExcelFileFinder fileFinder = new ExcelFileFinder();
 
-        Raport1.Generate(MockDataset.GetMockTaskRecordList());
+        List<String> stringList = fileFinder.findExcelFiles("/var/home/student/Downloads/");
+        List<TaskRecord> recordData = new ArrayList<>();
+        for (String s: stringList) {
+            System.out.println(s);
+            loader.loadXml(s);
+            recordData.addAll(loader.getRecords());
+            System.out.println();
+
+        }
+        System.out.println(recordData.size());
+        for (TaskRecord t: recordData) {
+            System.out.println(t);
+        }
+
+        Raport1.Generate(recordData);
 
         System.out.println("Błędy:");
-        for (String e: logger.getReportErrors()             ) {
+        for (String e: logger.getReportErrors()) {
             System.out.println(e);
         }
         System.out.println("Raport:");
-        for (String l: logger.getReportLines()             ) {
+        for (String l: logger.getReportLines()) {
             System.out.println(l);
         }
-
-        loader.loadXml("/var/home/student/Documents/reporter-dane/reporter-dane/2012/01/Kowalski_Jan.xls");
-
-        for (TaskRecord record : loader.getRecords()) {
-            System.out.println(record.getDevelopername());
-            System.out.println(record.getFolderMonth());
-            System.out.println(record.getGetFolderYear());
-            System.out.println(record.getTaskName());
-            System.out.println(record.getProjectName());
-            System.out.println(record.getProjectDate());
-            System.out.println(record.getWorkHours());
-
-            System.out.println();
-            System.out.println();
-        }
-
-
 
     }
 }
