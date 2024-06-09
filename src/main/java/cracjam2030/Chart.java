9fada3261b328556b1aca6f9b4d09b0251ba0b4c
Amaz <sonicmephit@gmail.com>
2024-06-09 14:39:54 +0200

    last final

--- src/main/java/cracjam2030/Chart.java
+++ src/main/java/cracjam2030/Chart.java
@@ -11,16 +11,25 @@ import org.knowm.xchart.style.Styler.LegendPosition;
 
 public class Chart {
 
-    public static void generateChart( String chartType) {
-
-        switch (chartType) {
-            case "1": Chart1.generateChart(Main.logger);
-            break;
-            case "2": Chart2.generateChart(Main.logger);
-                break;
-            case "3": Chart3.generateChart(Main.logger);
-                break;
+    public static void generateChart(String chartType) {
+        try {
+            switch (chartType) {
+                case "1":
+                    Chart1.generateChart(Main.logger);
+                    break;
+                case "2":
+                    Chart2.generateChart(Main.logger);
+                    break;
+                case "3":
+                    Chart3.generateChart(Main.logger);
+                    break;
+            }
         }
+        catch (Exception e)
+        {
+            Main.logger.addError(e.getMessage());
+        }
+
     }
 
 }
