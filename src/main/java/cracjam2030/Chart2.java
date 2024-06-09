059061dc7bf83404255edca7c3c65ecc6198a8a8
student <maryniak@student.agh.edu.pl>
2024-06-09 13:01:46 +0200

    Funkcjonalność wykresów

--- src/main/java/cracjam2030/Chart2.java
+++ src/main/java/cracjam2030/Chart2.java
@@ -1,55 +1,30 @@
 package cracjam2030;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.TreeMap;
 
-import com.sun.source.tree.Tree;
 import org.knowm.xchart.CategoryChart;
 import org.knowm.xchart.CategoryChartBuilder;
 import org.knowm.xchart.SwingWrapper;
 import org.knowm.xchart.demo.charts.ExampleChart;
 import org.knowm.xchart.style.Styler.LegendPosition;
 
-import static cracjam2030.Main.logger;
-
-public class Chart implements ExampleChart<CategoryChart> {
+public class Chart2 implements ExampleChart<CategoryChart> {
 
     private List<String> keys;
     private List<Double> values;
 
 
-    public Chart(List<String>  keys, List<Double> values) {
+    public Chart2(List<String>  keys, List<Double> values) {
         this.keys = keys;
         this.values = values;
     }
 
-
-
-    public static void generateChart1(MessageLogger logger) {
-        List<String> keys = new ArrayList<String>(logger.getRaport1Data().keySet());;
-        List<Double> values = new ArrayList<Double>(logger.getRaport1Data().values());
-
-        ExampleChart<CategoryChart> exampleChart = new Chart(keys, values);
-        CategoryChart chart = exampleChart.getChart();
-        new SwingWrapper<>(chart).displayChart();
-    }
-
-    public static void generateChart2(MessageLogger logger) {
+    public static void generateChart(MessageLogger logger) {
         List<String> keys = new ArrayList<String>(logger.getRaport2Data().keySet());;
         List<Double> values = new ArrayList<Double>(logger.getRaport2Data().values());
 
-        ExampleChart<CategoryChart> exampleChart = new Chart(keys, values);
-        CategoryChart chart = exampleChart.getChart();
-        new SwingWrapper<>(chart).displayChart();
-    }
-
-    public static void generateChart3(MessageLogger logger) {
-        List<String> keys = new ArrayList<String>(logger.getRaport3Data().keySet());;
-        List<Double> values = new ArrayList<Double>(logger.getRaport3Data().values());
-
-        ExampleChart<CategoryChart> exampleChart = new Chart(keys, values);
+        ExampleChart<CategoryChart> exampleChart = new Chart1(keys, values);
         CategoryChart chart = exampleChart.getChart();
         new SwingWrapper<>(chart).displayChart();
     }
@@ -66,8 +41,8 @@ public class Chart implements ExampleChart<CategoryChart> {
                         .width(800)
                         .height(600)
                         .title(getClass().getSimpleName())
-                        .xAxisTitle("Score")
-                        .yAxisTitle("Number")
+                        .xAxisTitle("Developers")
+                        .yAxisTitle("Hours")
                         .build();
 
         // Customize Chart
